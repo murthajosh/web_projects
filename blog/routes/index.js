73665b270632513e251e578698e4e73086f8ed64
Josh Murtha <murthajosh@gmail.com>
2020-05-06 13:00:08 -0500

    Added post.handlebars elements to index.js dictionary. Used app.use and expres.static('public') to try to inject CSS into the template.

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -11,3 +11,17 @@ module.exports = router;
 router.get('/test', function(req, res, next) {
   res.send('Message Test');
 });
+
+router.get('/post', function(req, res, next) {
+  app.use('/public', express.static('public'));
+  res.render('post', 
+             {title:'Blog Title',
+              tagline: 'Blog Tagline',
+              recent_posts: ['post1','post2','post3'],
+              post_title: 'Post Title',
+              post_subtitle: 'Post Subtitle',
+              publish_date: 'October 25, 1988',
+              author_name: 'Josh Murtha',
+              }
+            );
+});
